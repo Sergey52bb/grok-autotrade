62521337c1b8575b8a67b9101060422d1ec9ffa3
Sergey52bb <149534508+Sergey52bb@users.noreply.github.com>
2026-02-28 10:45:12 +0300

    Update lightweight-charts.js

--- lightweight-charts.js
+++ lightweight-charts.js
@@ -1,1 +1,13 @@
-<script src="https://unpkg.com/lightweight-charts@4.1.1/dist/lightweight-charts.standalone.production.js"></script>
+// Example content of lightweight-charts.js
+var LightweightCharts = (function() {
+  // contents of the library...
+  return {
+    createChart: function(container, options) {
+      // chart creation logic
+    },
+    addCandlestickSeries: function(options) {
+      // adding candlestick series logic
+    }
+    // ... other functions
+  };
+})();
